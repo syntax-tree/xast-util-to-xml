d2f26483769ff80681f59530ce81e42dd377f4d6
Titus Wormer <tituswormer@gmail.com>
2023-01-17 14:12:27 +0100

    Add tests for exposed identifiers

--- test/core.js
+++ test/core.js
@@ -2,8 +2,15 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import {u} from 'unist-builder'
 import {toXml} from '../index.js'
+import * as mod from '../index.js'
 
 test('toXml()', () => {
+  assert.deepEqual(
+    Object.keys(mod).sort(),
+    ['toXml'],
+    'should expose the public api'
+  )
+
   assert.throws(
     () => {
       // @ts-expect-error runtime.
